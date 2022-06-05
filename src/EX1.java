9ca3abc9f9e6c48db123040451eb1d1ed9bafa48
MusaHesenov <musa.hesenov.2018@gmail.com>
2022-06-05 10:33:31 +0400

    Exercise 1 solved

---
+++ src/EX1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class EX1 {
+
+    public static  void stdin_stdout(){
+    Scanner scan = new Scanner(System.in);
+    String st = scan.nextLine();
+    int a = scan.nextInt();
+
+    System.out.println("myString is " +st );
+    System.out.println("myInt is " +a );
+}
+    public static void main(String[] args) {
+        stdin_stdout();
+    }
+}
